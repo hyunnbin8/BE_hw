8d4659f8b0e73ccca44fa6337917cf9c76f7747a
hyunnbin8 <bin1025yh@gmail.com>
2025-03-25 21:41:25 +0900

    [0325] 양현빈

---
+++ wordcount/wordcount_prj/wordcount/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def word_count(request):
+    return render(request, 'word_count.html')
+
+def result(request):
+    entered_text = request.GET['fulltext']
+    word_list = entered_text.split()
+
+    word_dictionary = {}
+
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+
+    result = sum(word_dictionary.values())
+
+    except_text = entered_text.replace(" ", "")
+
+    max_value = max(word_dictionary.values())
+
+    return render(request, 'result.html', {'alltext': entered_text, 'dictionary': word_dictionary.items(), 'word_sum': result, 'except_word': except_text, 'max_value': max_value})
+
+def hello(request):
+    user_name = request.GET['fulltext']
+    return render(request, 'hello.html', {'name': user_name})
